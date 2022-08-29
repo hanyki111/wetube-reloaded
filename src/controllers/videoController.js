f8ab682298c1d6d6661d3b89de8ce6e31f89c635
hanyki111 <shy617@naver.com>
2022-08-30 01:30:28 +0900

    #6.1 Array Database part Two (10:11)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,39 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-  ];
   res.render("home.pug", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-  return res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  console.log(id);
+
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const edit = (req, res) => {
   console.log(req.params);
